6af32f8ebe3c86cd9e484d4c7516f3da0052bd94
AwesoMeme <belkadean@mail.ru>
2021-03-20 17:19:01 +0700

    Add and test matrixMultiplication-func()

--- seven_lab/main.cpp
+++ seven_lab/main.cpp
@@ -4,72 +4,11 @@
 #include <random>
 #include "omp.h"
 
-
-//template<typename T>
-//class matrix
-//{
-//public:
-
-//    int columns = 0;
-//    int rows = 0;
-//    matrix(int rows, int columns)
-//    {
-//        this->rows = rows;
-//        this->columns = columns;
-
-//        multiplicationMatrix.resize(rows, std::vector<T>(columns));
-//        doThings();
-//    }
-//    const std::vector<T> *operator[](int i) const {
-//        return multiplicationMatrix[i];
-//    }
-//    void doThings();
-//    void fillMatrix();
-//    void printMatrix();
-//    void matrixMultiplication();
-
-//};
-
-
-//template<typename T>
-//void matrix<T>::doThings()
-//{
-//    fillMatrix();
-//}
-
-
-//template<typename T>
-
-
-
-
 ////число столбцов первой матрицы = числу строк второй матрицы
 ////у результирующей матрицы столько строк, сколько в первой матрице
 ////и столько столбцов, сколько во второй
 
-//template<typename T>
-//void matrixMultiplication(matrix<double> firstMatrix, matrix<double> secondMatrix)
-//{
-//    int i = 0, j = 0, k = 0;
-//    int rows = firstMatrix.rows; //строки первой матрицы
-//    int columns = secondMatrix.columns; //столбцы второй
-//    int secondMatrixrows = secondMatrix.rows; //строки второй
-
-//    std::vector<std::vector<T>> result(rows, std::vector<T>(columns));
-//    //идём по строкам первой матрицы
-//    //умножаем результат на столбцы второй матрицы
-//    for(i = 0; i < rows; i++){
-//        for(j = 0; j < columns; j++){
-//            for(k = 0; k < secondMatrixrows; k++){
-//                result[i][j] += firstMatrix[i][j]*secondMatrix[j][k];
-//            }
-//        }
-//    }
-//}
-
-
-
-void printMatrix(std::vector<std::vector<double>>&matrix)
+void printMatrix(std::vector<std::vector<double>> & matrix)
 {
     for(int i = 0; i < matrix.size(); i++){
         for(int j = 0; j < matrix.at(0).size(); j++){
@@ -77,7 +16,30 @@ void printMatrix(std::vector<std::vector<double>>&matrix)
         }
         std::cout<<std::endl;
     }
+    std::cout<<std::endl;
 }
+
+
+void matrixMultiplication(std::vector<std::vector<double>> & firstMatrix, std::vector<std::vector<double>> & secondMatrix)
+{
+    int i = 0, j = 0, k = 0;
+    int rows = firstMatrix.size(); //строки первой матрицы
+    int columns = secondMatrix.at(0).size(); //столбцы второй
+    int secondMatrixRows = secondMatrix.size(); //строки второй
+
+    std::vector<std::vector<double>> result(rows, std::vector<double>(columns));
+    //идём по строкам первой матрицы
+    //умножаем результат на столбцы второй матрицы
+    for(i = 0; i < rows; i++){
+        for(j = 0; j < columns; j++){
+            for(k = 0; k < secondMatrixRows; k++){
+                result[i][j] += firstMatrix[i][k]*secondMatrix[k][j];
+            }
+        }
+    }
+    printMatrix(result);
+}
+
 
 void fillMatrix(std::vector<std::vector<double>>&matrix)
 {
@@ -92,19 +54,45 @@ void fillMatrix(std::vector<std::vector<double>>&matrix)
     printMatrix(matrix);
 }
 
+
 int main()
 {
     int firstMatrixRows = 0, firstMatrixCols = 0;
     int secondMatrixRows = 0, secondMatrixCols = 0;
-    std::cout<<"Input the rows and columns of the first matrix"<<std::endl;
-    std::cin>>firstMatrixRows>>firstMatrixCols;
-    std::cout<<"Input the rows and columns of the second matrix"<<std::endl;
-    std::cin>>secondMatrixRows>>secondMatrixCols;
+    bool isEqual = false;
+
+    while(!isEqual){
+        std::cout<<"Input the rows and columns of the first matrix"<<std::endl;
+        std::cin>>firstMatrixRows>>firstMatrixCols;
+
+        std::cout<<"Input the rows and columns of the second matrix"<<std::endl;
+        std::cin>>secondMatrixRows>>secondMatrixCols;
+
+        if(secondMatrixRows != firstMatrixCols){
+            std::cout<<"Incorrect size!"<<std::endl;
+        }
+        else{
+            isEqual = true;
+        }
+    }
+
+
+
+
     std::vector<std::vector<double>> firstMatrix(firstMatrixRows, std::vector<double>(firstMatrixCols));
     std::vector<std::vector<double>> secondMatrix(secondMatrixRows, std::vector<double>(secondMatrixCols));
+
+    std::cout<<"The first matrix: "<<std::endl;
     fillMatrix(firstMatrix);
     std::cout<<std::endl;
+
+    std::cout<<"The second matrix: "<<std::endl;
     fillMatrix(secondMatrix);
+    std::cout<<std::endl;
+
+    std::cout<<"Result of multiplication is: "<<std::endl;
+    matrixMultiplication(firstMatrix, secondMatrix);
+
     system("pause");
     return 0;
 }
